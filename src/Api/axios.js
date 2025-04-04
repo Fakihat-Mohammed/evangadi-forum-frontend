089d47598c8432ad2f36616e43dee22c5e9650b8
Fakihat-Mohammed <fakihmoh44@gmail.com>
2025-04-05 00:00:45 +0300

    final

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const axiosInstance = axios.create({
   // baseURL: "http://localhost:2017/api",
-  baseURL: "https://evangadi-forum-backend-1-gvn4.onrender.com/",
+  baseURL: "https://evangadi-forum-backend-1-gvn4.onrender.com/api",
 });
 
 export default axiosInstance;
